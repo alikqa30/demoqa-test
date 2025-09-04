3ae5d841891153fb2fb9260bcc6e38f70a07557e
alikqa30 <aliksloyan1993@gmail.com>
2025-09-04 11:08:07 +0700

    1. add workflows configuration. 2.add Step annotation. 3.fix and edit test

--- src/test/java/demoQa/examplePageObject/pages/RegistrationPage.java
+++ src/test/java/demoQa/examplePageObject/pages/RegistrationPage.java
@@ -1,13 +1,18 @@
 package demoQa.examplePageObject.pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import demoQa.examplePageObject.pages.components.CalendarComponent;
 import demoQa.examplePageObject.pages.components.FileUploadComponent;
 import demoQa.examplePageObject.pages.components.RegistrationModulResult;
 import demoQa.examplePageObject.pages.components.SubmitComponent;
+import io.qameta.allure.Step;
+
 import java.io.File;
-import static com.codeborne.selenide.Condition.text;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -22,11 +27,17 @@ public class RegistrationPage {
             firstNameInput = $("#firstName"),
             lastNameInput = $("#lastName"),
             emailInput = $("#userEmail"),
+            genderCheckBox = $("#genterWrapper"),
             phoneInput = $("#userNumber"),
-            currentAddressInput = $("#currentAddress");
+    //bd
+            subjectInput = $("#subjectsInput"),
+            hobbiesCheckBox = $("#hobbiesWrapper"),
+    //file
+            currentAddressInput = $("#currentAddress"),
+            state = $("#state"),
+            city = $("#city");
 
-
-
+    @Step("Открываем форму регистрации")
     public RegistrationPage openPage() {
         open("/automation-practice-form");
         Selenide.executeJavaScript("$(\"#fixedban\").remove");
@@ -34,80 +45,101 @@ public class RegistrationPage {
 
         return this;
     }
+    @Step("Устанавливаем имя")
     public RegistrationPage setFirstName(String value) {
         firstNameInput.setValue(value);
 
         return this;
     }
+    @Step("Устанавливаем фамилию")
     public RegistrationPage setLastName(String value) {
         lastNameInput.setValue(value);
 
         return this;
     }
+    @Step("Устанавливаем электронную почту")
     public RegistrationPage setEmail(String value) {
         emailInput.setValue(value);
 
         return this;
     }
+    @Step("Устанавливаем пол")
     public RegistrationPage setGender(String value) {
-        $("#gender-radio-1").parent().$(byText(value)).click();
+        genderCheckBox.parent().$(byText(value)).click();
 
         return this;
     }
+    @Step("Устанавливаем телефон")
     public RegistrationPage setPhone(String value) {
         phoneInput.setValue(value);
 
         return this;
     }
+    @Step("Устанавливаем дату рождения")
     public RegistrationPage setBirthDate(String day, String month, String year) {
-        $("#dateOfBirthInput").click();
+        $("#dateOfBirthInput").shouldBe(visible,enabled).click();
         calendarComponent.setDate( day,  month, year);
-
         return this;
     }
+    @Step("Устанавливаем предметы")
     public RegistrationPage setSubjects(String value) {
-        $("#subjectsInput").setValue((value)).pressEnter();
+        $(".overlay, .loader").shouldBe(Condition.hidden);
+        subjectInput.shouldBe(visible, enabled).setValue((value)).pressEnter();
 
         return this;
     }
+    @Step("Устанавливаем хобби")
     public RegistrationPage setHobbies(String value) {
-        $("label[for = hobbies-checkbox-3]").parent().$(byText(value)).click();
+        $(".overlay, .loader").shouldBe(Condition.hidden);
+        hobbiesCheckBox.$$("label.custom-control-label").findBy(Condition.text(value)).shouldBe(visible, enabled)
+                .scrollTo().click();
 
         return this;
     }
+    @Step("Загружаем файл")
     public RegistrationPage fileDownload(File file) {
         fileUploadComponent.pictureToDownload(file);
 
         return this;
     }
+    @Step("Устанавливаем текущий адрес")
     public RegistrationPage setCurrentAddress(String value) {
         currentAddressInput.scrollTo().setValue(value);
 
         return this;
     }
-    public RegistrationPage setStateAndCity (String state, String city) {
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText(state)).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText(city)).click();
+    @Step("Устанавливаем Штат")
+    public RegistrationPage setState(String value) {
+        state.click();
+        $("#stateCity-wrapper").$(byText(value)).click();
+        return this;
+    }
+    @Step("Устанавливаем город")
+    public RegistrationPage setCity (String value) {
+        city.click();
+        $("#stateCity-wrapper").$(byText(value)).click();
 
         return this;
     }
+    @Step("Нажимаем на кнопку")
     public RegistrationPage clickOnSubmit() {
         $("#submit").click();
 
         return this;
     }
+    @Step("Верифицируем модальное окно")
     public RegistrationPage verifyResultModuleAppears() {
         registrationModulResult.verifyModuleAppears();
 
         return this;
     }
+    @Step("Верифицируем данных в модальном окне с данными ввода")
     public RegistrationPage verifyResult(String key, String value) {
         registrationModulResult.verifyResult(key, value);
 
         return this;
     }
+    @Step("Закрываем моадльное окно")
     public RegistrationPage closeLargeModal() {
         $("#closeLargeModal").click();
 
--- src/test/java/demoQa/examplePageObject/successfullyRegistrationsWithPageObjectsTest.java
+++ src/test/java/demoQa/examplePageObject/successfullyRegistrationsWithPageObjectsTest.java
@@ -1,30 +1,41 @@
 package demoQa.examplePageObject;
 
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class successfullyRegistrationsWithPageObjectsTest extends testBase {
 
     @Test
     void fieldAutomationPracticeForm()  {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         File file = new File("C:/Users/alikb/OneDrive/Рабочий стол/правила НИ/тауантинсуй");
 
         registrationPage.openPage()
                         .setFirstName(firstName)
                         .setLastName(lastName)
                         .setEmail(userEmail)
-                        .setGender("Male")
-                        .setPhone(userNumber)
-                        .setBirthDate("04", "September", "1993")
-                        .setSubjects("Economics")
-                        .setHobbies("Music")
-                        .setCurrentAddress(currentAddress);
+                        .setGender(userGender)
+                        .setPhone(String.valueOf(userNumber))
+                        .setBirthDate(day, month, String.valueOf(year));
+
+        registrationPage.setSubjects(userSubjects);
+
+        sleep(1000);
+
+        registrationPage.setHobbies(userHobbies);
+
+        registrationPage.setCurrentAddress(currentAddress);
 
         registrationPage.fileDownload(file)
 
-                        .setStateAndCity("NCR", "Delhi")
+                        .setState(userState)
+                        .setCity(userCity)
 
                         .clickOnSubmit();
 
@@ -32,9 +43,13 @@ public class successfullyRegistrationsWithPageObjectsTest extends testBase {
         registrationPage.verifyResultModuleAppears()
                 .verifyResult("Student Name", firstName + " " + lastName)
                 .verifyResult("Student Email", userEmail)
-                .verifyResult("Gender", "Male")
-                .verifyResult("Mobile", userNumber)
-                .verifyResult("Date of Birth", "04 September,1993")
+                .verifyResult("Gender", userGender)
+                .verifyResult("Mobile", String.valueOf(userNumber))
+                .verifyResult("Date of Birth", day + " " + month + "," +year)
+                .verifyResult("Subjects", userSubjects)
+                .verifyResult("Hobbies", userHobbies)
+                .verifyResult("Address", currentAddress)
+                .verifyResult("State and City", userState + " " + userCity)
 
                         .closeLargeModal();
     }
