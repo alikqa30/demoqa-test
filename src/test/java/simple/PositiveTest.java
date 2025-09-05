19ac92fab64cc07bc360503ad0d1097a524b5e68
alikqa30 <aliksloyan1993@gmail.com>
2025-09-05 16:10:08 +0700

    implementation via PageObject

---
+++ src/test/java/simple/PositiveTest.java
@@ -0,0 +1,31 @@
+package simple;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@Tag("simple")
+public class PositiveTest {
+
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test3() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test4() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/simple/SkippedTest.java
@@ -0,0 +1,23 @@
+package simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@Tag("simple")
+public class SkippedTest {
+
+    @Test
+    void test1() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void test2() {
+        assertTrue(false);
+    }
+}
